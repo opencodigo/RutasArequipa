95f8e6334e3199ca8c87fe7d65a7802f7b3fdb24
by Wilian Llano <38366996+opencodigo@users.noreply.github.com>
2019-07-19 10:59:21 -0500

    Add files via upload

---
+++ proyectoRutas/js/variables.js
@@ -0,0 +1,10 @@
+// Your web app's Firebase configuration
+export var firebaseConfig = {
+    apiKey: "AIzaSyCqZAos3AZMLN8X64Gprz46zE_GxBU9Iz4",
+    authDomain: "foodappperu.firebaseapp.com",
+    databaseURL: "https://foodappperu.firebaseio.com",
+    projectId: "foodappperu",
+    storageBucket: "gs://foodappperu.appspot.com/", //storage de imagenes 
+    messagingSenderId: "230470254681",
+    appId: "1:230470254681:web:a1aa1bc41a17947c"
+  };
