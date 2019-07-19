c5741239dff42fcf4fa4b0918fc7d88e13580a42
by Wilian Llano <38366996+opencodigo@users.noreply.github.com>
2019-07-19 11:04:01 -0500

    Update variables.js

--- proyectoRutas/js/variables.js
+++ proyectoRutas/js/variables.js
@@ -1,10 +1,10 @@
 // Your web app's Firebase configuration
 export var firebaseConfig = {
     apiKey: "",
-    authDomain: "foodappperu.firebaseapp.com",
-    databaseURL: "https://foodappperu.firebaseio.com",
-    projectId: "foodappperu",
-    storageBucket: "gs://foodappperu.appspot.com/", //storage de imagenes 
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "", //storage de imagenes 
     messagingSenderId: "230470254681",
     appId: "1:230470254681:web:a1aa1bc41a17947c"
   };
